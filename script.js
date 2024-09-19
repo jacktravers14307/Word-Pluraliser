cde76f0ebfd5a377e5d7909ccd10af8acdb45c76
JackT_ <120268899+jacktravers14307@users.noreply.github.com>
2024-09-19 11:00:43 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 function pluralthing2() {
-    // Get the input value
+
     const word = document.getElementById("userInput").value;
     const pluralOutput = document.getElementById("pluralOutput");
 
-    // Check if the input is a string
     if (typeof word === "string" && word.trim() !== "") {
-        // Convert to lowercase for checking
         const lowerWord = word.toLowerCase();
 
         if (lowerWord.endsWith("sh") || lowerWord.endsWith("s")) {
